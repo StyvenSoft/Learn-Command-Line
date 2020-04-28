f78805d0047d8881ee57cf4917649b2b9b3d976f
Steveen <steveen.echeverri@gmail.com>
2020-04-28 17:49:43 -0500

    Lesson Completed Navigation

---
+++ Lesson-1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+	# Este es nuestro primer progrma
+	echo Hola Mundo
+
+# El comando ls es muy útil para ver los archivos y directorios 
+# que tenemos dentro del directorio en el que estamos
+
+$ ls 
+
+# navegar el sistema de archivos desde la línea de comandos
+
+$ pwd 
+
+# CD (Change Directory) sirve para mostrar el nombre del directorio actual y, 
+# también, permite cambiar de directorio. 
+
+$ cd name_directory
+
+# Imprima el directorio de trabajo nuevamente para ver la nueva ubicación.
+
+$ cd ..
+
+# Liste todos los archivos y directorios en el directorio de trabajo.
+
+$ cd ../feb
+
+#  mkdir tiene su origen en las palabras make subdirectory que significan crear subdirectorio en inglés.
+
+$ mkdir media 
+
+# El comando touch se usa principalmente para crear archivos vacíos y cambiar marcas de tiempo de archivos o carpetas
+
+$ touch keyboard.txt
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Learn-Command-Line
+
+Discover the power of this simple yet essential text-based tool and increase your productivity as a developer.
+
+The command line is a text interface for your computer. It’s a program that takes in commands, which it passes on to the computer’s operating system to run.
+
+
+## LESSON 1 Navigating the File System
+
+Begin using the command line by learning how to navigate directories and files.
+
+Exercises
+
+- [x] 1. Your First Command
+
+- [x] 2. ls
+
+- [x] 3. Filesystem
+
+![File screen](File.PNG)
+
+- [ ] 4. pwd
+
+- [ ] 5. cd I
+
+- [ ] 6. cd II
+
+- [ ] 7. mkdir
+
+- [ ] 8. touch
