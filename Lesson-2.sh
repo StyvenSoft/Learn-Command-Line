88b6bc9268e94dca2a21587eaced946fb0f47256
Steveen <steveen.echeverri@gmail.com>
2020-05-09 18:53:39 -0500

    Lesson 2 Completed Manipulation

--- Lesson-2.sh
+++ Lesson-2.sh
@@ -23,6 +23,40 @@
 
 $ ls -alt 
 
-## El cpcomando copia archivos o directorios. Aquí, copiamos el contenido de frida.txt en lincoln.txt .
+## El cp comando copia archivos o directorios. Aquí, copiamos el contenido de frida.txt en lincoln.txt .
 
 $ cp name-file1 name-file2
+
+## Para copiar varios archivos en un directorio, use cpuna lista de archivos de origen como primeros 
+# argumentos y el directorio de destino como último argumento
+
+$ cp carpeta/archivo1.txt carpeta/archivo2.txt carpeta-destino/
+
+# Los *selecciona todos los archivos en el directorio de trabajo
+# Estos caracteres especiales se llaman comodines .
+
+$ cp * carpeta/
+
+# m*.txt selecciona todos los archivos en el directorio de trabajo que comienzan con "m" y terminan con ".txt", y los copia en carpeta / 
+
+$ cp m*.txt carpeta/
+
+# El mv comando mueve archivos.
+
+$ mv file.txt carpeta/
+
+# Para mover un archivo a un directorio, utilícelo mvcon el archivo de origen como primer argumento y el directorio de destino como segundo argumento
+
+$ mv file1.txt file2.txt carpeta-destino/
+
+## Para cambiar el nombre de un archivo, úselo mv con el archivo antiguo como primer argumento y el nuevo archivo como segundo argumento
+
+$ mv name.txt new-name.txt
+
+## El rm comando elimina archivos y directorios. Aquí eliminamos el archivo waterboy.txt del sistema de archivos.
+
+$ rm file.txt
+
+# La -r es una opción que modifica el comportamiento del rmcomando. El -r significa “recursivo”, y se usa para borrar un directorio y todos sus directorios secundarios.
+
+$ rm -r removes directories
